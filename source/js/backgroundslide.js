40079f27123537a81c38a148d7559017771dbfb2
Dongju20 <114079607+Dongju20@users.noreply.github.com>
2024-07-13 13:38:54 +0900

    메인화면 구현(90프로..?)

--- source/js/backgroundslide.js
+++ source/js/backgroundslide.js
@@ -7,12 +7,22 @@ $(document).ready(function () {
 function move(){
     //슬라이드를 움직일 요소를 가져옴..
     //첫번째 이미지를 가져옵니다
-    console.log("이동합니다...");
-    let img = $('.img-slide:first');
-    console.log(img.width());
-    let length = img.height();
-    console.log(length);
-    img.animate({
-        top: `-=${length * 3}`
-    },50);
+    let imgBottom = $('.img-slide:eq(1)');
+    let length = imgBottom.height();
+    //이미지 바텀위치를 설정....
+    imgBottom.css("transform", `translateY(-${length}px)`);
+    let imgTop = $('.img-slide:eq(0)');
+    imgTop.animate({
+       top:  `${length}`
+    },2500);
+    imgBottom.animate({
+        top: `${length}`
+    },2500,function(){
+        let imgBottom = $('.img-slide:eq(1)');
+        let imgTop = $('.img-slide:eq(0)');
+        imgTop.insertBefore(imgBottom);
+        imgBottom.css('top','0');
+        imgTop.css('top','0');
+        move();
+    });
 }
