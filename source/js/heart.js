4ad8bd1079c061279d92064c30eb35748a15947f
Dongju20 <114079607+Dongju20@users.noreply.github.com>
2024-07-17 17:44:23 +0900

    체력 로직 작성

--- source/js/heart.js
+++ source/js/heart.js
@@ -1,5 +1,5 @@
-let heart;
-
+let MAX_HEART;
+let current_heart;
 $(document).ready(function () {
     //플레이어 정보 가져오기
     let player_info = localStorage.getItem('player_info');
@@ -10,9 +10,30 @@ $(document).ready(function () {
 
     let heart_charac = character_info[player_info.current_player].hp;
     let svg = $('.heart').find('svg');
-    heart = heart_charac;
+    current_heart = heart_charac;
+    MAX_HEART = heart_charac;
     console.log(svg);
     for(let i = heart_charac; i < 10; i++){
         $(svg[i]).hide();
     } 
 });
+// 체력 추가 함수 jQuery.addHeart()를 호출하면 됨...
+jQuery.addHeart = function(){
+    if(current_heart == MAX_HEART)
+        return;
+    else{
+        let svg = $('.heart').find('svg');
+        $(svg[current_heart]).attr('fill','red');
+        current_heart++;
+    }   
+}
+
+//체력 감소 함수
+jQuery.minusHeart = function(){
+       current_heart--;
+       let svg = $('.heart').find('svg');
+       $(svg[current_heart]).attr('fill','black');
+       if(current_heart == 0){
+            window.open('./end.html', '_self');
+       }
+}
