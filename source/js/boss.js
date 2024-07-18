1cced1d8f2b2a055ec05e2e245b878eb0185b649
Hoon1999 <kjhoon44@gmail.com>
2024-07-18 18:00:09 +0900

    add: meteor_caution function

--- source/js/boss.js
+++ source/js/boss.js
@@ -2,8 +2,13 @@ var boss = null;
 var time_limit;
 var way;
 
+var boss_appear_ani_dur = 0.5 ; // 보스 출현 속도 조절
+var meteor_war_ani_dur = 2 ; // 운석 경고등 시간 조절
+var meteor_fall_ani_dur = 5; // 운석 낙하 시간 조절
+
 function init_stage_boss(difficulty) {
-    if (boss != null) { boss = null; }
+    if (boss != null) { delete_boss(); }
+    if(difficulty == null || !(0 < difficulty && difficulty < 6)) { return 0; }
 
     let field = document.querySelector('.slider-container');
     let link = document.querySelector('#boss-css');
@@ -15,7 +20,13 @@ function init_stage_boss(difficulty) {
         attack_pattern_1_timer_id: null, // summon dragons pattern
         attack_pattern_2_timer_id: null, // meteor fall pattern
     }
-    $(link).attr('href', boss.shape);
+    if(difficulty == 5) {
+        boss.shape = "boss3.css";
+    }
+    $(link).attr('href', boss.shape); // 아 이게 너무 늦게 load가 됨..
+    // 위 코드가 너무 늦게 load가 되니까 이후 코드들이 먼저 실행되서 꼬여버림.
+    // 동기식 처리를 할 수 있도록 바꾸는게 좋은데 할 줄 몰라서 그냥 set timeout 1초로 줌.
+    // 문제는 $(link).attr('href', boss.shape) 가 1초 넘게 걸리면, set timeout 1초로 준게 무용지물이 됨.
     setTimeout(function () {
         boss.top = 0 - boss_layout.offsetHeight;
         boss.left = field.offsetWidth / 2 - boss_layout.offsetWidth / 2;
@@ -24,7 +35,7 @@ function init_stage_boss(difficulty) {
             top: boss.top,
             visibility: 'visible',
             transform: 'translatey(' + (0 - boss.top / 2) + 'px)',
-            transition: '0.5s'
+            transition: boss_appear_ani_dur + 's'
         });
     }, 1000);
 
@@ -47,27 +58,35 @@ function time_limit_timer() {
         setTimeout(time_limit_timer, 1000);
     } else if (time_limit == 0) {
         delete_boss();
-        game_over(); // 플레이어 패배
+        // 게임 오버
+        window.open('./end.html', '_self');
     }
 }
-
+function meteor_caution() {
+    for (let i = 0; i < 5; ++i) {
+        if (i != way) $('.meteor-warning')[i].style.display = 'block';
+        $('.meteor-warning')[i].classList.add('active');
+        setTimeout(function() {
+            $('.meteor-warning')[i].classList.remove('active');
+            $('.meteor-warning')[i].style.display = 'none';
+        }, meteor_war_ani_dur * 1000)
+    }
+}
 function meteor_fall() {
     way = Math.floor(Math.random() * 100 % 5); // 운석이 안떨어지는 위치
     $('.meteor').css('display', 'none');
     for (let i = 0; i < 5; ++i) {
         if ($('.meteor')[i].classList.contains('active')) {
             $('.meteor')[i].classList.remove('active');
-            setTimeout(meteorfunc, 1000);
-        }
-        else {
-            setTimeout(meteorfunc, 1000);
         }
     }
+    meteor_caution();
+    setTimeout(meteorfunc, meteor_war_ani_dur * 1000);
 }
 
 function meteorfunc() {
     let temp = setInterval(collision_detection, 100);
-    setTimeout(() => { clearInterval(temp) }, 5 * 1000);
+    setTimeout(() => { clearInterval(temp) }, meteor_fall_ani_dur * 1000);
     for (let i = 0; i < 5; ++i) {
         if (i != way) $('.meteor')[i].style.display = 'block';
         $('.meteor')[i].classList.add('active');
@@ -92,7 +111,7 @@ function clear_stage() {
     gold = gold + 100 * difficulty
     if (difficulty < 5) {
         difficulty++;
-    }
+    } 
 }
 function collision_detection() {
     let hero = document.querySelector('#hero').getBoundingClientRect();
@@ -108,6 +127,8 @@ function collision_detection() {
             if (l <= hero_coordinate[i][0] && hero_coordinate[i][0] <= r) {
                 if (t <= hero_coordinate[i][1] && hero_coordinate[i][1] <= b) {
                     console.log('충돌발생');
+                    document.querySelectorAll('#meteor-zone > .meteor-line > img')[j].style.display = 'none';
+                    // jQuery.minusHeart(); // 체력 감소
                 }
             }
         }
